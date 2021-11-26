763b13bfa1b88f274f84786b974e4fe70714c664
juancruzgardey <juancruzgardey@hotmail.com>
2021-11-26 19:03:47 -0300

    add functions to work with dates

--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -8,12 +8,22 @@ export const range = (start, end) => {
 
 export const dateToISOString = (date) => date.toISOString().slice(0, 10);
 
+// returns a string with a dd/mm/yyyy pattern
 export const dateToLocalString = (date) =>
   `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 
-export const stringToDate = (aString) =>
-  new Date(
+// converts a dd/mm/yyyy string to Date
+export const stringToDate = (aString) => {
+  console.log(
+    new Date(
+      aString.slice(6, 10),
+      parseInt(aString.slice(3, 5) - 1),
+      aString.slice(0, 2)
+    )
+  );
+  return new Date(
     aString.slice(6, 10),
     parseInt(aString.slice(3, 5) - 1),
     aString.slice(0, 2)
   );
+};
