b2a09449a5206da63235fc87e9daceec53f22f92
juancruzgardey <juancruzgardey@hotmail.com>
2021-11-26 19:02:54 -0300

    add new form constraints

--- frontend/src/utils/formConstraints.js
+++ frontend/src/utils/formConstraints.js
@@ -18,4 +18,14 @@ export const formConstraints = {
     },
     message: 'The value must be integer',
   },
+  regex: {
+    validate: (value, options) => value === '' || options.pattern.test(value),
+  },
+  date: {
+    validate: (value) => {
+      const ddmmyyyyPattern = /^(?=\d)(?:(?:31(?!.(?:0?[2469]|11))|(?:30|29)(?!.0?2)|29(?=.0?2.(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(?:\x20|$))|(?:2[0-8]|1\d|0?[1-9]))([-.\/])(?:1[012]|0?[1-9])\1(?:1[6-9]|[2-9]\d)?\d\d(?:(?=\x20\d)\x20|$))?(((0?[1-9]|1[012])(:[0-5]\d){0,2}(\x20[AP]M))|([01]\d|2[0-3])(:[0-5]\d){1,2})?$/;
+      return value === '' || ddmmyyyyPattern.test(value);
+    },
+    message: 'Invalid date',
+  },
 };
