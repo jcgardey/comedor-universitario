db01e051bed1f340a345f243497d2727cca13bbb
juancruzgardey <juancruzgardey@hotmail.com>
2021-12-01 17:12:38 -0300

    update webpack configuration

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 module.exports = {
   module: {
     rules: [
@@ -5,9 +7,18 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'static/frontend'),
+    filename: 'main.js',
+  },
 };
