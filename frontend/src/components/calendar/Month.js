2faf47aeb9450e7527cc8d6c80c759d054b1f76e
juancruzgardey <juancruzgardey@hotmail.com>
2021-12-03 19:24:22 -0300

    add navigation between months

--- frontend/src/components/calendar/Month.js
+++ frontend/src/components/calendar/Month.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CalendarDay from './CalendayDay';
 import styled from 'styled-components';
 import colors from '../../styles/colors';
 import { range } from '../../utils/common';
+import { Button, FlexContainer } from '../Layout';
 
 const MonthGrid = styled.div`
   display: grid;
@@ -13,12 +14,25 @@ const MonthGrid = styled.div`
 `;
 
 const MonthName = styled.p`
+  text-align: center;
   font-size: 1.4em;
   font-weight: 600;
-  color: ${colors.red};
+  color: ${colors.lightred};
+`;
+
+const ControlButton = styled(Button)`
+  color: ${colors.lightred};
+  background-color: transparent;
+  margin: 0 0.5em;
+`;
+
+const MonthControls = styled(FlexContainer)`
+  align-items: center;
 `;
 
 export const Month = ({ year, month, onDaySelection }) => {
+  const [current, setCurrent] = useState([month, year]);
+
   const months = [
     'Enero',
     'Febrero',
@@ -34,30 +48,64 @@ export const Month = ({ year, month, onDaySelection }) => {
     'Diciembre',
   ];
 
-  const monthFirstDay = new Date(year, month, 1);
-  const previousMonday = monthFirstDay.getDate() - (monthFirstDay.getDay() - 1);
+  const SATURDAY = 6;
+  const SUNDAY = 0;
 
-  const monthLastDay = new Date(year, month + 1, 0);
-  const nextFriday = monthLastDay.getDate() + (5 - monthLastDay.getDay());
+  const [currentMonth, currentYear] = current;
 
   const isWeekend = (day) =>
-    new Date(year, month, day).getDay() == 6 ||
-    new Date(year, month, day).getDay() == 0;
+    new Date(currentYear, currentMonth, day).getDay() == SATURDAY ||
+    new Date(currentYear, currentMonth, day).getDay() == SUNDAY;
+
+  const monthFirstDay = new Date(currentYear, currentMonth, 1);
+  const startDay =
+    monthFirstDay.getDate() -
+    (!isWeekend(monthFirstDay.getDate()) ? monthFirstDay.getDay() - 1 : 0);
+
+  const monthLastDay = new Date(currentYear, currentMonth + 1, 0);
+  const endDay =
+    monthLastDay.getDate() +
+    (monthLastDay.getDay() !== SUNDAY ? 7 - monthLastDay.getDay() : 0);
+
+  const DECEMBER = 11;
+  const JANUARY = 0;
+
+  const nextMonth = () =>
+    setCurrent(
+      currentMonth === DECEMBER
+        ? [JANUARY, currentYear + 1]
+        : [currentMonth + 1, currentYear]
+    );
+
+  const previousMonth = () =>
+    setCurrent(
+      currentMonth === JANUARY
+        ? [DECEMBER, currentYear - 1]
+        : [currentMonth - 1, currentYear]
+    );
 
   return (
     <>
-      <MonthName>
-        {months[month]} {year}
-      </MonthName>
+      <MonthControls>
+        <ControlButton onClick={previousMonth}>
+          <i className="fas fa-angle-left fa-2x"></i>
+        </ControlButton>
+        <MonthName>
+          {months[currentMonth]} {currentYear}
+        </MonthName>
+        <ControlButton onClick={nextMonth}>
+          <i className="fas fa-angle-right fa-2x"></i>
+        </ControlButton>
+      </MonthControls>
       <MonthGrid>
-        {range(previousMonday, nextFriday)
+        {range(startDay, endDay)
           .filter((day) => !isWeekend(day))
           .map((day, i) => (
             <CalendarDay
-              key={month - i}
-              month={month}
+              key={currentMonth - i}
+              month={currentMonth}
               day={day}
-              year={year}
+              year={currentYear}
               onDaySelection={onDaySelection}
             />
           ))}
