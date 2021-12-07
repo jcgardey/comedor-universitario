3bbbf7104347c719bf3d509c04682c58db35a2e2
juancruzgardey <juancruzgardey@hotmail.com>
2021-12-07 17:26:47 -0300

    refactor menuOnSale API

---
+++ menus/api/menu_on_sale.py
@@ -0,0 +1,33 @@
+from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from menus.models import Menu, MenuOnSale, Site, MenuAlreadyOnSaleException
+from django.utils.dateparse import parse_date
+from menus.serializers import MenuOnSaleSerializer
+
+
+class CreateMenuOnSaleAPI(APIView):
+
+    def post(self, request):
+        site = Site.objects.get(pk=request.data['site'])
+        try:
+            menu_sales = self.create_sale(request.data, site)
+        except MenuAlreadyOnSaleException:
+            return Response({'message': 'menu already on sale on the date', 'error': 'menu-already-on-sale' }, status=status.HTTP_400_BAD_REQUEST)
+        return Response(MenuOnSaleSerializer(menu_sales, many=True).data)
+    
+    def create_sale(self, sale_data, site):
+        menu = Menu.objects.get(pk=sale_data['menu'])
+        sale_date = parse_date(sale_data['sale_date'])
+        return menu.create_sale(sale_date, sale_data['stock'] , site)
+   
+
+class ListMenuOnSaleAPI(generics.ListAPIView):
+    serializer_class = MenuOnSaleSerializer
+
+    def get_queryset(self):
+        filterFields = {}
+        if self.request.query_params.get('sale_date', None) is not None:
+            filterFields['sale_date'] = parse_date(self.request.query_params.get('sale_date'))
+        return MenuOnSale.objects.filter(**filterFields)
--- menus/urls.py
+++ menus/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import MenuAPI, CreateMenuOnSaleAPI, MenuComponentViewSet
+from .views import MenuAPI, MenuComponentViewSet
+from menus.api.menu_on_sale import CreateMenuOnSaleAPI, ListMenuOnSaleAPI
 
 router = routers.DefaultRouter()
 router.register('menu_component', MenuComponentViewSet, basename='MenuComponent')
 
 urlpatterns = [
     path('api/menu', MenuAPI.as_view()),
-    path('api/menu_on_sale', CreateMenuOnSaleAPI.as_view()),
+    path('api/menus_on_sale/new', CreateMenuOnSaleAPI.as_view()),
+    path('api/menus_on_sale', ListMenuOnSaleAPI.as_view()),
     path('api/', include(router.urls))
 ]
